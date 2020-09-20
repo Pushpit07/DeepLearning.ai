94185793684f488f8bb2da585e43b5d19fdca6e6
pushpitbhardwaj <pushpit07@gmail.com>
2020-09-20 09:00:12 +0530

    Convolutions updated

--- convolutions.py
+++ convolutions.py
@@ -19,3 +19,51 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 model.summary()
 model.fit(training_images, training_labels, epochs=5)
 test_loss = model.evaluate(test_images, test_labels)
+
+"""
+# GRADED FUNCTION: train_mnist_conv
+def train_mnist_conv():
+    # Please write your code only where you are indicated.
+    # please do not remove model fitting inline comments.
+
+    # YOUR CODE STARTS HERE
+    class myCallback(tf.keras.callbacks.Callback):
+      def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('acc')>0.998):
+          print("Reached 99.8% accuracy so cancelling training!")
+          self.model.stop_training = True
+            
+    callbacks = myCallback()
+    # YOUR CODE ENDS HERE
+
+    mnist = tf.keras.datasets.mnist
+    (training_images, training_labels), (test_images, test_labels) = mnist.load_data(path=path)
+    # YOUR CODE STARTS HERE
+    training_images=training_images.reshape(60000, 28, 28, 1)
+    training_images=training_images / 255.0
+    test_images = test_images.reshape(10000, 28, 28, 1)
+    test_images=test_images/255.0
+    # YOUR CODE ENDS HERE
+
+    model = tf.keras.models.Sequential([
+            # YOUR CODE STARTS HERE
+            tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
+            tf.keras.layers.MaxPooling2D(2, 2),
+              tf.keras.layers.Flatten(),
+              tf.keras.layers.Dense(128, activation='relu'),
+              tf.keras.layers.Dense(10, activation='softmax')
+            # YOUR CODE ENDS HERE
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # model fitting
+    history = model.fit(
+        # YOUR CODE STARTS HERE
+        training_images, training_labels, epochs=20, callbacks=[callbacks]
+        # YOUR CODE ENDS HERE
+    )
+    # model fitting
+    return history.epoch, history.history['acc'][-1]
+
+
+"""

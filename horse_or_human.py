879a694bc65388cc0bfdcadac9c44175248be73d
pushpitbhardwaj <pushpit07@gmail.com>
2020-09-22 21:20:11 +0530

    Cats and Dogs classifier

--- horse_or_human.py
+++ horse_or_human.py
@@ -97,7 +97,7 @@ train_datagen = ImageDataGenerator(rescale=1/255)
 # Flow training images in batches of 128 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
         '/tmp/horse-or-human/',  # This is the source directory for training images
-        target_size=(300, 300),  # All images will be resized to 150x150
+        target_size=(300, 300),  # All images will be resized to 300X300
         batch_size=128,
         # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')

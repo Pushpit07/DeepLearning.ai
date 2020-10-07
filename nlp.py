660b4d48ad79de4aec4d8ec29e3af3456b7c8641
pushpitbhardwaj <pushpit07@gmail.com>
2020-10-07 16:30:40 +0530

    Padded NLP

--- nlp.py
+++ nlp.py
@@ -9,10 +9,18 @@ sentences = [
 	'Do you think that a dog is amazing?'
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = 100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index)
 
 sequences = tokenizer.texts_to_sequences(sentences)
 print(sequences)
+
+test_data = [
+	'I really love a cat',
+	'My dog loves my Manatee'
+]
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
